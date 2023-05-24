e542b418ed2208ee8afc526cdc1382a11f227e08
meweez <meweez@meweezs-MacBook-Pro.local>
2023-05-25 01:21:20 +0330

    update

--- asset-collector.sh
+++ asset-collector.sh
@@ -2,7 +2,7 @@
 
 
 
-GITHUB_TOKEN="Your github token"
+GITHUB_TOKEN=""
 
 
 
@@ -23,8 +23,8 @@ printf "
  \__,_|___/___/\___|\__|     \___\___/|_|_|\___|\___|\__\___/|_|   
                                                                    
 
-			                           ${cyan}Developed by MHA${NC}	     			                  
-                                                            ${yellow}mha4065.com${NC}              		               
+                                                   ${cyan}Developed by MHA${NC}                                           
+                                                            ${yellow}mha4065.com${NC}                                          
 
 "
 
@@ -60,40 +60,14 @@ if ! type "jq" > /dev/null; then
   exit
 fi
 
-if ! command -v "mapcidr" > /dev/null; then
-  echo -e "  ${red}[-]${NC} mapcidr is not installed ! Please install it and try again."
-  exit
-fi
-
-if ! command -v "dnsx" > /dev/null; then
-  echo -e "  ${red}[-]${NC} dnsx is not installed ! Please install it and try again."
-  exit
-fi
-
-if ! type "dig" > /dev/null; then
-  echo -e "  ${red}[-]${NC} dig is not installed ! Please install it and try again."
-  exit
-fi
-
 if ! type "curl" > /dev/null; then
   echo -e "  ${red}[-]${NC} cURL is not installed ! Please install it and try again."
-  exit
-fi
-
-if ! type "openssl" > /dev/null; then
-  echo -e "  ${red}[-]${NC} OpenSSL is not installed ! Please install it and try again."
   exit
 fi
 
 if ! command -v subfinder &> /dev/null
 then
     echo -e "  ${red}[-]${NC} subfinder could not be found !"
-    exit
-fi
-
-if ! command -v assetfinder &> /dev/null
-then
-    echo -e "  ${red}[-]${NC} assetfinder could not be found !"
     exit
 fi
 
@@ -120,13 +94,7 @@ echo
 echo -e "${blue}[!]${NC} Subdomain enumeration :"
 
 echo -e "  ${green}[+]${NC} subfinder"
-subfinder -d $domain -all -o results/$domain/subfinder.txt
-#=======================================================================
-
-# Run assetfinder and save results to file
-#=======================================================================
-echo -e "  ${green}[+]${NC} assetfinder"
-assetfinder --subs-only $domain > results/$domain/assetfinder.txt
+subfinder -d $domain -all -o results/$domain/subfinder.txt &> /dev/null
 #=======================================================================
 
 #crt.sh on Domain
@@ -141,20 +109,31 @@ query=$(cat <<-END
             plainto_tsquery('certwatch', '$domain') @@ identities(ci.CERTIFICATE)
 END
 )
-echo "$query" | psql -t -h crt.sh -p 5432 -U guest certwatch | sed 's/ //g' | egrep ".*.\.$domain" | sed 's/*\.//g' | tr '[:upper:]' '[:lower:]' | sort -u | uniq | tee -a results/$domain/crtsh.txt
-# curl -s "https://crt.sh/?q=$domain&output=json" | tr '\0' '\n' | jq -r ".[].common_name,.[].name_value" | sort -u | uniq | tee -a results/$domain/crtsh.txt
+echo "$query" | psql -t -h crt.sh -p 5432 -U guest certwatch | sed 's/ //g' | egrep ".*.\.$domain" | sed 's/*\.//g' | tr '[:upper:]' '[:lower:]' | sort -u | uniq | tee -a results/$domain/crtsh.txt &> /dev/null
+# curl -s "https://crt.sh/?q=$domain&output=json" | tr '\0' '\n' | jq -r ".[].common_name,.[].name_value" | sort -u | uniq | tee -a results/$domain/crtsh.txt &> /dev/null
 #=======================================================================
 
 #Subdomain enumeration in Github
 #=======================================================================
-echo -e "  ${green}[+]${NC} Github"
-github-subdomains -d $domain -e -o results/$domain/github.txt -t $GITHUB_TOKEN
+if [[ ! -z "$GITHUB_TOKEN" ]]
+then
+  echo -e "  ${green}[+]${NC} Github"
+  github-subdomains -d $domain -e -o results/$domain/github.txt -t $GITHUB_TOKEN &> /dev/null
+fi
+#=======================================================================
+
+
+#Subdomain enumeration in SourceGraph
+#=======================================================================
+echo -e "  ${green}[+]${NC} SourceGraph"
+q=$(echo $domain| sed -e 's/\./\\\./g')
+src search -json '([a-z\-]+)?:?(\/\/)?([a-zA-Z0-9]+[.])+('${q}') count:5000 fork:yes archived:yes' | jq -r '.Results[] | .lineMatches[].preview, .file.path' | grep -oiE '([a-zA-Z0-9]+[.])+('${q}')' | awk '{ print tolower($0) }' | sort -u uniq | tee -a results/$domain/sourcegraph.txt
 #=======================================================================
 
 #Subdomain enumeration using AbuseDB
 #=======================================================================
 echo -e "  ${green}[+]${NC} AbuseDB"
-curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: Chrome" | grep -E '<li>\w.*</li>' | sed -E 's/<\/?li>//g' | sed -e "s/$/.$domain/" | tee -a results/$domain/abusedb.txt
+curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: Chrome" | grep -E '<li>\w.*</li>' | sed -E 's/<\/?li>//g' | sed -e "s/$/.$domain/" | tee -a results/$domain/abusedb.txt &> /dev/null
 #=======================================================================
 
 # Combine all results and remove duplicates
@@ -162,7 +141,7 @@ curl -s "https://www.abuseipdb.com/whois/$domain" -H "user-agent: Chrome" | grep
 echo -e "${blue}[!]${NC} Combining results and removing duplicates..."
 cat results/$domain/*.txt | sort -u > results/$domain/all_subs.txt
 
-rm results/$domain/abusedb.txt results/$domain/github.txt results/$domain/crtsh.txt results/$domain/subdomain_certificate.txt results/$domain/assetfinder.txt results/$domain/subfinder.txt 
+# rm results/$domain/abusedb.txt results/$domain/github.txt results/$domain/crtsh.txt results/$domain/subdomain_certificate.txt results/$domain/assetfinder.txt results/$domain/subfinder.txt results/$domain/sourcegraph.txt
 echo -e "${blue}[!]${NC} Subdomain enumeration complete! Results saved to results/$domain/all_subs.txt :))"
 #=======================================================================
 
@@ -172,32 +151,8 @@ cat results/$domain/all_subs.txt | dnsx -resp-only -silent | dnsx -silent -ptr >
 cat results/$domain/all_subs.txt | dnsx -txt -silent >> results/$domain/resource.txt
 #=======================================================================
 
-# Name resolution of subdomains and remove CDN IP's
+# Name resolution of subdomains 
 #=======================================================================
-echo -e "${blue}[!]${NC} Performing name resolution and filtering CDN IP's..."
-cat results/$domain/all_subs.txt | dnsx -silent -resp-only | tee results/$domain/res_subs.txt
-cat results/$domain/res_subs.txt | mapcidr -filter-ip cdns.txt | tee results/$domain/ips.txt
+echo -e "${blue}[!]${NC} Performing name resolution"
+cat results/$domain/all_subs.txt | dnsx -silent -resp-only | sort -u | tee results/$domain/all_subs_ip.txt  &> /dev/null
 #=======================================================================
-
-# Scanning port on No CDN IPs
-#=======================================================================
-echo -e "${blue}[!]${NC} Scanning IPs for opening ports..."
-
-cat results/$domain/ips.txt | naabu -silent | tee results/$domain/portscan.txt
-#=======================================================================
-
-# Certificate search again
-#=======================================================================
-echo -e "${blue}[!]${NC} Certificate Search again on IP and open ports..."
-while read ip_port; do
-  ipport_cert=$(echo | openssl s_client -showcerts -connect $ip_port 2>/dev/null | openssl x509 -inform pem -noout -text)
-  if [ $ipport_cert != "Could not read certificate from <stdin>" ]; then
-    san2=$(echo "$ipport_cert" | grep -A1 "X509v3 Subject Alternative Name:" | tail -n1)
-    san_urlss=$(echo "$san2" | grep -o "DNS:[^,]*" | sed 's/DNS://g' | xargs -n1 | sed 's/^\s*//' | sed 's/\s*$//')
-    for i in "${san_urlss[@]}"; do
-      if [ "$i" != "$domain" ]; then
-        echo $san_urlss | tee -a results/$domain/resource.txt
-      fi
-    done
-  fi
-done < results/$domain/portscan.txt
